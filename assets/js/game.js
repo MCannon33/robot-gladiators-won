d83e74f7d3f0235187506a2bd9e6c0245b2eb4ce
MCannon33 <marialcannon@gmail.com>
2020-10-29 09:18:34 -0400

    setup array

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,12 @@ var playerMoney = 10;
 // You can also log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth);
 
-var enemyName = "Roborto";
+var enemyNames = ["Roborto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
+cconsole.log(enemyNames.length);
 
-var fight = function() {
+ var fight = function() {
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Gladiators!");
     
@@ -62,4 +63,4 @@ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? En
   }
 };
 
-fight();
+//fight();
