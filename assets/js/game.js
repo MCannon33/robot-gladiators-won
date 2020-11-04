b12c48801769fa18a4226c1fd1de43e3282b593e
MCannon33 <marialcannon@gmail.com>
2020-11-04 01:35:09 -0500

    feature/initial-game

--- assets/js/game.js
+++ assets/js/game.js
@@ -8,10 +8,14 @@ var enemyHealth = 50;
 var enemyAttack = 12;
 
 var fight = function(enemyName) { 
-  console.log(enemyNames.length, i++) //fight function statements
+  console.log(enemyNames.length, i++) 
+  // repeat and execute as long as the enemy-robot is alive 
+  while(enemyHealth > 0) {
   
+  //fight function statements
+
   // Alert players that they are starting the round
-    window.alert("Welcome to Robot Gladiators!");
+    //window.alert("Welcome to Robot Gladiators!");
 
     //Skip the fight 
 var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? Enter 'FIGHT' or 'SKIP' to choose.");
@@ -59,8 +63,11 @@ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? En
       fight();
     }
   }
+}
 };
   for(var i = 0; i < enemyNames.length; i++) {
-    fight(enemyNames[i]);
+    var pickedEnemyName = enemyNames[i];
+    enemyHealth = 50;
+    fight(pickedEnemyName);
   }
 fight();
