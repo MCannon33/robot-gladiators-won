74302c051f92eb4e0fdc604b379e17325bc77d09
MCannon33 <marialcannon@gmail.com>
2020-11-04 01:57:41 -0500

    update skip function

--- assets/js/game.js
+++ assets/js/game.js
@@ -9,8 +9,8 @@ var enemyAttack = 12;
 
 var fight = function(enemyName) { 
   console.log(enemyNames.length, i++) 
-  // repeat and execute as long as the enemy-robot is alive 
-  while(enemyHealth > 0) {
+  // repeat and execute as long as the enemy-robot & player-robot is alive 
+  while(enemyHealth > 0 && playerHealth > 0) {
   
   //fight function statements
 
@@ -31,6 +31,7 @@ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? En
     // check enemy's health
     if (enemyHealth <= 0) {
       window.alert(enemyName + " has died!");
+      break;
     } else {
       window.alert(enemyName + " still has " + enemyHealth + " health left.");
     }
@@ -44,6 +45,7 @@ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? En
     // check player's health
     if (playerHealth <= 0) {
       window.alert(playerName + " has died!");
+      break;
     } else {
       window.alert(playerName + " still has " + playerHealth + " health left.");
     }
@@ -56,7 +58,9 @@ var promptFight = window.prompt("Would you like to FIGHT or SKIP this battle? En
     if (confirmSkip) {
       window.alert(playerName + " has decided to skip this fight. Goodbye!");
       // subtract money from playerMoney for skipping
-      playerMoney = playerMoney - 2;
+      playerMoney = playerMoney - 10;
+      console.log("playerMoney", playerMoney);
+      break;
     }
     // if no (false), ask question again by running fight() again
     else {
