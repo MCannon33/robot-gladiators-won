69e1eabd18fbd5c7fe92a7048017bb776a06738c
MCannon33 <marialcannon@gmail.com>
2020-11-09 20:18:27 -0500

    jss updates

--- assets/js/game.js
+++ assets/js/game.js
@@ -70,9 +70,14 @@ var fight = function(enemyName) {
   for(var i = 0; i < enemyNames.length; i++) {
     if(playerHealth > 0){
       window.alert("Welcome to Robot Gladiators! Round " + (i + 1));
-    }
+    } 
     var pickedEnemyName = enemyNames[i];
     enemyHealth = 50;
     fight(pickedEnemyName);
+  }
+  for (var i = 0; i < enemyNames.length; i++) {
+    debugger;
+    // call fight function with enemy-robot
+    fight(enemyNames[i]);
   }
 fight();
